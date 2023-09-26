0b62133dae4aeea226dd83c3415eebf9767b4d66
taro <taro@example.com>
2023-09-27 00:09:29 +0900

    first commit

---
+++ src/main/java/com/example/monthlydrive/MonthlydriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.monthlydrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlydriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlydriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/monthlydrive/MonthlydriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monthlydrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlydriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
